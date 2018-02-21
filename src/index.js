5498c4d28e7072763fa6429ff10cffdf6dd07ee4
feelalaska <36642377+feelalaska@users.noreply.github.com>
2018-02-21 14:15:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,19 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count = 0;
+  for (var i=0; i<preferences.length; i++) {
+	  if ((preferences[i] != i+1)&&(preferences[i] != 0)) {
+		  // i - position of 1th love-triangle element in array
+		  var j = preferences[i]-1; //position of 2nd love-triangle element in array
+		  var k = preferences[j]-1; //position of 3rd love-triangle element in array
+		  if ((j+1 === preferences[i])&&(k+1 === preferences[j])&&(i+1 === preferences[k])) {
+			  preferences[i] = 0;
+			  preferences[j] = 0;
+			  preferences[k] = 0;
+			  count++;
+    	}
+	  }
+  }
+  return count;
 };
